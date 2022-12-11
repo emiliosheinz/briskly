f7313098bd44b66d4546c3a8344f52debfdd8c20
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-12-11 14:57:27 -0300

    chore: adds prettier configuration

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,15 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-};
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  singleQuote: true,
+  endOfLine: 'lf',
+  printWidth: 80,
+  semi: false,
+  tabWidth: 2,
+}
