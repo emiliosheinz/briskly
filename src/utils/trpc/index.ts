6aeecae15a3cbecf776fdacf279f169fc7406932
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-12-12 19:09:04 -0300

    refactor: uses isServerSide util in trpc

--- src/utils/trpc/index.ts
+++ src/utils/trpc/index.ts
@@ -4,9 +4,10 @@ import { type inferRouterInputs, type inferRouterOutputs } from '@trpc/server'
 import superjson from 'superjson'
 
 import { type AppRouter } from '~/server/trpc/router/_app'
+import { isServerSide } from '~/utils/runtime'
 
 const getBaseUrl = () => {
-  if (typeof window !== 'undefined') return '' // browser should use relative url
+  if (!isServerSide) return '' // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}` // dev SSR should use localhost
 }
