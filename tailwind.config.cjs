3229044ee35a22d6d34ec4a0a514da234f798f18
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-12-19 11:07:03 -0300

    chore: :wrench: adds brand colors to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,25 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      primary: {
+        900: '#0F172A',
+        400: '#94A3B8',
+        50: '#F8FAFC',
+      },
+      error: {
+        700: '#B91C1C',
+        300: '#FCA5A5',
+      },
+      success: {
+        700: '#15803D',
+        300: '#86EFAC',
+      },
+      warning: {
+        500: '#EAB308',
+        200: '#FEF08A',
+      },
+    },
   },
   plugins: [],
 }
