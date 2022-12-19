dc7c0e624d6f54d35a383e7d0aaa3aa209724087
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-12-19 17:38:33 -0300

    fix: :bug: call function

--- src/utils/trpc/index.ts
+++ src/utils/trpc/index.ts
@@ -7,7 +7,7 @@ import { type AppRouter } from '~/server/trpc/router/_app'
 import { isServerSide } from '~/utils/runtime'
 
 const getBaseUrl = () => {
-  if (!isServerSide) return '' // browser should use relative url
+  if (!isServerSide()) return '' // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}` // dev SSR should use localhost
 }
