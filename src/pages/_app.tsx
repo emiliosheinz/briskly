d44e101b5179de0b9ff25216452d46dc3b33c891
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-12-20 08:17:11 -0300

    feat: :sparkles: adds vercel analytics to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import { type Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
 import { type AppType } from 'next/app'
@@ -13,6 +14,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   )
 }
