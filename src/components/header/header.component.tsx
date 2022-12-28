e497d3b0e1cbf892c6f32153d038bf73e1f2e0d2
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-12-27 21:32:16 -0300

    style: :lipstick: updates header width and image behavior

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -21,18 +21,17 @@ export function Header() {
   }
 
   return (
-    <header className='relative bg-primary-50'>
-      <div className='mx-auto max-w-full px-4 sm:px-6'>
-        <div className='flex items-center justify-between border-b-2 border-primary-200 py-6 md:justify-start md:space-x-10'>
+    <header className='relative border-b-2 border-primary-200 bg-primary-50'>
+      <div className='mx-auto max-w-7xl px-4 sm:px-6'>
+        <div className='flex items-center justify-between  py-6 md:justify-start md:space-x-10'>
           <div className='flex justify-start lg:w-0 lg:flex-1'>
             <Link href='/'>
               <span className='sr-only'>Briskly</span>
               {/* TODO emiliosheinz: Change image component to use custom component */}
               <Image
-                className='h-8 w-auto sm:h-10'
                 src='/images/logo.png'
-                width={80}
-                height={80}
+                width={48}
+                height={48}
                 alt='Briskly logo'
               />
             </Link>
