81b362f608e7f91b0ce4681c0078b7491175c9da
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-12-28 10:30:48 -0300

    refactor: :recycle: moves toBase64 to its own file

--- src/components/image/image.component.tsx
+++ src/components/image/image.component.tsx
@@ -2,7 +2,7 @@ import { memo } from 'react'
 
 import NextImage from 'next/image'
 
-import { isServerSide } from '~/utils/runtime'
+import { toBase64 } from '~/utils/conversion'
 
 import type { ImageProps } from './image.types'
 
@@ -10,9 +10,6 @@ const shimmer = `
 <svg version="1.1" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
   <rect width="100%" height="100%" fill="#E2E8F0" />
 </svg>`
-
-const toBase64 = (str: string) =>
-  isServerSide() ? Buffer.from(str).toString('base64') : window.btoa(str)
 
 export function BaseImage(props: ImageProps) {
   return (
