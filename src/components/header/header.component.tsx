dd0c2734f7a6bda4265fa0e2c4220c1820340a17
Emilio Heinzmann <emiliosheinz@gmail.com>
2023-01-02 13:49:24 -0300

    feat: :sparkles: adds loading validation to render login button or account access

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,28 +1,48 @@
+import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 
 import { Image } from '~/components/image'
 
+import { AuthButton } from '../auth-button'
 import { DesktopPrimaryMenu } from './components/desktop-primary-menu'
 import { DesktopSecondaryMenu } from './components/desktop-secondary-menu '
 import { MobileHamburgerMenu } from './components/mobile-hamburger-menu'
 
 export function Header() {
+  const { data: session, status } = useSession()
+
+  const isLoadingSession = status === 'loading'
+
   const renderLogo = () => (
     <Image src='/images/logo.png' width={40} height={35} alt='Briskly logo' />
   )
+
+  const renderDesktopSecondaryMenu = () => {
+    if (isLoadingSession) return null
+
+    if (!session) {
+      return (
+        <div className='hidden w-32 md:block'>
+          <AuthButton />
+        </div>
+      )
+    }
+
+    return <DesktopSecondaryMenu />
+  }
 
   return (
     <header className='border-b-2 border-primary-200 bg-primary-50'>
       <div className='mx-auto flex max-w-7xl items-center justify-between px-4 py-6 sm:px-6 md:space-x-10'>
         <div className='flex items-center justify-start'>
-          <Link href='/'>
+          <Link href='/' data-testid='home-anchor'>
             <span className='sr-only'>Briskly</span>
             {renderLogo()}
           </Link>
           <DesktopPrimaryMenu />
         </div>
         <MobileHamburgerMenu />
-        <DesktopSecondaryMenu />
+        {renderDesktopSecondaryMenu()}
       </div>
     </header>
   )
