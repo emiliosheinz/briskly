8a168e5621258ae2701515855c9a4203963a6950
Emilio Heinzmann <emiliosheinz@gmail.com>
2023-01-02 19:52:15 -0300

    refactor: :recycle: uses custom button to create auth button

--- src/components/auth-button/auth-button.component.tsx
+++ src/components/auth-button/auth-button.component.tsx
@@ -1,21 +1,22 @@
 import { signIn, signOut, useSession } from 'next-auth/react'
 
+import { Button } from '../button'
+
 export function AuthButton() {
   const { data: session } = useSession()
 
   const onClick = session ? () => signOut() : () => signIn()
   const label = session ? 'Sair' : 'Login'
-  const sessionBasedClassNames = session
-    ? 'text-error-700 border-error-700'
-    : 'text-primary-900 border-primary-900'
+  const sessionBasedVariant = session ? 'bad' : 'secondary'
 
   return (
-    <button
-      className={`bg-transparent w-full rounded border py-2 px-4 font-semibold ${sessionBasedClassNames}`}
+    <Button
+      fullWidth
       data-testid='auth-button'
+      variant={sessionBasedVariant}
       onClick={onClick}
     >
       {label}
-    </button>
+    </Button>
   )
 }
