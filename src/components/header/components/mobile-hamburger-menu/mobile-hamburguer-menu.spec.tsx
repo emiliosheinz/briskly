0f164d9ca945ad90fcd91f3df9dbb72bae588d49
Emilio Heinzmann <emiliosheinz@gmail.com>
2023-01-02 12:05:38 -0300

    test: :white_check_mark: adds session based tests for mobile menu

--- src/components/header/components/mobile-hamburger-menu/mobile-hamburguer-menu.spec.tsx
+++ src/components/header/components/mobile-hamburger-menu/mobile-hamburguer-menu.spec.tsx
@@ -5,44 +5,97 @@ import { MOBILE_MENU_OPTIONS } from '../../header.constants'
 import { MobileHamburgerMenu } from './mobile-hambuger-menu.component'
 
 describe('Header MobileHamburgerMenu Component', () => {
-  it('should be correctly rendered', () => {
-    const component = render(<MobileHamburgerMenu />)
+  describe('Authenticated', () => {
+    it('should be correctly rendered', () => {
+      const component = render(
+        withNextAuth(<MobileHamburgerMenu />, 'authenticated'),
+      )
 
-    expect(component.asFragment()).toMatchSnapshot()
+      expect(component.asFragment()).toMatchSnapshot()
+    })
+
+    it('should be visible only on mobile', () => {
+      render(withNextAuth(<MobileHamburgerMenu />, 'authenticated'))
+
+      const component = screen.getByTestId('mobile-hamburger-menu')
+
+      expect(component).toHaveClass('md:hidden')
+      expect(component).not.toHaveClass('hidden')
+    })
+
+    it('should initially render only the popover button', () => {
+      render(withNextAuth(<MobileHamburgerMenu />, 'authenticated'))
+
+      expect(screen.getByTestId('popover-button')).toBeInTheDocument()
+      expect(screen.queryByTestId('popover-panel')).not.toBeInTheDocument()
+    })
+
+    it('should render popover panel when popover button is clicked', () => {
+      render(withNextAuth(<MobileHamburgerMenu />, 'authenticated'))
+
+      fireEvent.click(screen.getByTestId('popover-button'))
+
+      expect(screen.queryByTestId('popover-panel')).toBeInTheDocument()
+    })
+
+    it('should render all MOBILE_MENU_OPTIONS menus inside popover panel', () => {
+      render(withNextAuth(<MobileHamburgerMenu />, 'authenticated'))
+
+      fireEvent.click(screen.getByTestId('popover-button'))
+      const panel = screen.queryByTestId('popover-panel')
+
+      for (const { label } of MOBILE_MENU_OPTIONS.filter(
+        ({ isAuthRequired }) => !isAuthRequired,
+      )) {
+        expect(panel?.innerHTML).toContain(label)
+      }
+    })
   })
 
-  it('should be visible only on mobile', () => {
-    render(<MobileHamburgerMenu />)
+  describe('Unauthenticated', () => {
+    it('should be correctly rendered', () => {
+      const component = render(
+        withNextAuth(<MobileHamburgerMenu />, 'unauthenticated'),
+      )
 
-    const component = screen.getByTestId('mobile-hamburger-menu')
+      expect(component.asFragment()).toMatchSnapshot()
+    })
 
-    expect(component).toHaveClass('md:hidden')
-    expect(component).not.toHaveClass('hidden')
-  })
+    it('should be visible only on mobile', () => {
+      render(withNextAuth(<MobileHamburgerMenu />, 'unauthenticated'))
 
-  it('should initially render only the popover button', () => {
-    render(<MobileHamburgerMenu />)
+      const component = screen.getByTestId('mobile-hamburger-menu')
 
-    expect(screen.getByTestId('popover-button')).toBeInTheDocument()
-    expect(screen.queryByTestId('popover-panel')).not.toBeInTheDocument()
-  })
+      expect(component).toHaveClass('md:hidden')
+      expect(component).not.toHaveClass('hidden')
+    })
 
-  it('should render popover panel when popover button is clicked', () => {
-    render(withNextAuth(<MobileHamburgerMenu />, 'authenticated'))
+    it('should initially render only the popover button', () => {
+      render(withNextAuth(<MobileHamburgerMenu />, 'unauthenticated'))
 
-    fireEvent.click(screen.getByTestId('popover-button'))
+      expect(screen.getByTestId('popover-button')).toBeInTheDocument()
+      expect(screen.queryByTestId('popover-panel')).not.toBeInTheDocument()
+    })
 
-    expect(screen.queryByTestId('popover-panel')).toBeInTheDocument()
-  })
+    it('should render popover panel when popover button is clicked', () => {
+      render(withNextAuth(<MobileHamburgerMenu />, 'unauthenticated'))
 
-  it('should render all MOBILE_MENU_OPTIONS menus inside popover panel', () => {
-    render(withNextAuth(<MobileHamburgerMenu />, 'authenticated'))
+      fireEvent.click(screen.getByTestId('popover-button'))
 
-    fireEvent.click(screen.getByTestId('popover-button'))
-    const panel = screen.queryByTestId('popover-panel')
+      expect(screen.queryByTestId('popover-panel')).toBeInTheDocument()
+    })
 
-    for (const { label } of MOBILE_MENU_OPTIONS) {
-      expect(panel?.innerHTML).toContain(label)
-    }
+    it('should render all MOBILE_MENU_OPTIONS that dont require authentication inside popover panel', () => {
+      render(withNextAuth(<MobileHamburgerMenu />, 'authenticated'))
+
+      fireEvent.click(screen.getByTestId('popover-button'))
+      const panel = screen.queryByTestId('popover-panel')
+
+      for (const { label } of MOBILE_MENU_OPTIONS.filter(
+        ({ isAuthRequired }) => !isAuthRequired,
+      )) {
+        expect(panel?.innerHTML).toContain(label)
+      }
+    })
   })
 })
