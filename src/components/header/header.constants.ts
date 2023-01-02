0d5bd3a27bc8d86602501bc2db66eed7e343599a
Emilio Heinzmann <emiliosheinz@gmail.com>
2023-01-02 12:07:07 -0300

    feat: :sparkles: adds isAuthRequired prop to header menu options

--- src/components/header/header.constants.ts
+++ src/components/header/header.constants.ts
@@ -13,42 +13,49 @@ const MENU_OPTIONS = [
     label: 'Para Revisar',
     icon: ClockIcon,
     href: '#',
+    isAuthRequired: true,
   },
   {
     label: 'Mais Votados',
     icon: StarIcon,
     href: '#',
+    isAuthRequired: false,
   },
   {
     label: 'Mais Recentes',
     icon: FireIcon,
     href: '#',
+    isAuthRequired: false,
   },
   {
     label: 'Para Você',
     icon: HeartIcon,
     href: '#',
+    isAuthRequired: true,
   },
   {
     label: 'Criar Deck',
     icon: PlusCircleIcon,
     href: '#',
+    isAuthRequired: false,
   },
   {
     label: 'Meu Perfil',
     icon: UserCircleIcon,
     href: '#',
+    isAuthRequired: true,
   },
   {
     label: 'Configurações',
     icon: WrenchIcon,
     href: '#',
+    isAuthRequired: true,
   },
 ]
 
 export const MOBILE_MENU_OPTIONS = [...MENU_OPTIONS]
 
 export const DESKTOP_MENU_OPTIONS = {
-  PRIMARY: MENU_OPTIONS.slice(0, 5),
-  SECONDARY: MENU_OPTIONS.slice(5),
+  PRIMARY: MENU_OPTIONS.filter(({ isAuthRequired }) => !isAuthRequired),
+  SECONDARY: MENU_OPTIONS.filter(({ isAuthRequired }) => isAuthRequired),
 }
