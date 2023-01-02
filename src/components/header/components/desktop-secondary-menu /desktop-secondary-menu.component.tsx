6aef68b305058243f57e636b32424cacf653139a
Emilio Heinzmann <emiliosheinz@gmail.com>
2023-01-02 09:48:04 -0300

    chore: :technologist: adds data-testid to components

--- src/components/header/components/desktop-secondary-menu /desktop-secondary-menu.component.tsx
+++ src/components/header/components/desktop-secondary-menu /desktop-secondary-menu.component.tsx
@@ -10,8 +10,15 @@ import { DESKTOP_MENU_OPTIONS } from '../../header.constants'
 
 export function DesktopSecondaryMenu() {
   return (
-    <Popover as='div' className='relative hidden md:block'>
-      <Popover.Button className='inline-flex items-center justify-center bg-primary-50 p-1 text-primary-900'>
+    <Popover
+      data-testid='desktop-secondary-menu'
+      as='div'
+      className='relative hidden md:block'
+    >
+      <Popover.Button
+        data-testid='popover-button'
+        className='inline-flex items-center justify-center bg-primary-50 p-1 text-primary-900'
+      >
         <UserCircleIcon
           className='h-8 w-8 text-primary-900'
           aria-hidden='true'
@@ -26,7 +33,10 @@ export function DesktopSecondaryMenu() {
         leaveFrom='transform opacity-100 scale-100'
         leaveTo='transform opacity-0 scale-95'
       >
-        <Popover.Panel className='absolute right-0 mt-0 flex w-60 origin-top-right flex-col gap-3 rounded-md bg-primary-50 p-4 shadow-lg ring-1 ring-primary-900 ring-opacity-5'>
+        <Popover.Panel
+          data-testid='popover-panel'
+          className='absolute right-0 mt-0 flex w-60 origin-top-right flex-col gap-3 rounded-md bg-primary-50 p-4 shadow-lg ring-1 ring-primary-900 ring-opacity-5'
+        >
           {DESKTOP_MENU_OPTIONS.SECONDARY.map(option => (
             <Link
               href={option.href}
