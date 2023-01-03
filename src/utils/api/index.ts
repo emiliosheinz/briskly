6baddd205367fdf28f3231b3e49fb8558f19895e
Emilio Heinzmann <emiliosheinz@gmail.com>
2023-01-03 15:08:44 -0300

    refactor: :recycle: change trpc utils to api utils

---
+++ src/utils/api/index.ts
@@ -0,0 +1,38 @@
+import { httpBatchLink, loggerLink } from '@trpc/client'
+import { createTRPCNext } from '@trpc/next'
+import { type inferRouterInputs, type inferRouterOutputs } from '@trpc/server'
+import superjson from 'superjson'
+
+import { type AppRouter } from '~/server/trpc/router/_app'
+
+import { getBaseUrl } from '../runtime'
+
+export const api = createTRPCNext<AppRouter>({
+  config() {
+    return {
+      transformer: superjson,
+      links: [
+        loggerLink({
+          enabled: opts =>
+            process.env.NODE_ENV === 'development' ||
+            (opts.direction === 'down' && opts.result instanceof Error),
+        }),
+        httpBatchLink({
+          url: `${getBaseUrl()}/api/trpc`,
+        }),
+      ],
+    }
+  },
+  ssr: false,
+})
+
+/**
+ * Inference helper for inputs
+ * @example type HelloInput = RouterInputs['example']['hello']
+ **/
+export type RouterInputs = inferRouterInputs<AppRouter>
+/**
+ * Inference helper for outputs
+ * @example type HelloOutput = RouterOutputs['example']['hello']
+ **/
+export type RouterOutputs = inferRouterOutputs<AppRouter>
