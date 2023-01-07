bc3092f6671a29838d77fa1d6890f3d7aeb493d3
Emilio Heinzmann <emiliosheinz@gmail.com>
2023-01-07 10:40:43 -0300

    refactor: :recycle: changes server api folder structure due to better organization

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,9 +1,9 @@
 import { httpBatchLink, loggerLink } from '@trpc/client'
 import { createTRPCNext } from '@trpc/next'
-import { type inferRouterInputs, type inferRouterOutputs } from '@trpc/server'
+import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server'
 import superjson from 'superjson'
 
-import { type AppRouter } from '~/server/trpc/router/_app'
+import type { AppRouter } from '~/server/api/root'
 
 import { isServerSide } from '../runtime'
 
