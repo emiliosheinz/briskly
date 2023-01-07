a4b27ff998fd08c2e72cbb7812c277ffb4788597
Emilio Heinzmann <emiliosheinz@gmail.com>
2023-01-06 22:30:20 -0300

    feat: :sparkles: adds auth guard, header and container max-w to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,44 @@
-import { Analytics } from '@vercel/analytics/react'
-import { type Session } from 'next-auth'
-import { SessionProvider } from 'next-auth/react'
-import { type AppType } from 'next/app'
+import { Fragment } from 'react'
 
+import { Analytics } from '@vercel/analytics/react'
+import type { Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
+import type { AppType } from 'next/app'
+
+import { AuthGuard } from '~/components/auth-guard'
+import { Header } from '~/components/header'
+import type { WithAuthentication } from '~/types/auth'
 import { api } from '~/utils/api'
 
 import '~/styles/tailwind.css'
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+/**
+ * Needed to infer requiresAuthentication as a prop of Component
+ */
+type ComponentWithAuthentication<P> = P & {
+  Component: WithAuthentication
+}
+
+const MyApp: AppType<{ session: Session | null }> = props => {
+  const {
+    Component,
+    pageProps: { session, ...pageProps },
+  } = props as ComponentWithAuthentication<typeof props>
+
+  const OptionalAuthGuard = Component.requiresAuthentication
+    ? AuthGuard
+    : Fragment
+
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Header />
+      <main className='min-h-screen w-full bg-primary-50'>
+        <div className='m-auto w-full max-w-7xl p-3 md:p-5'>
+          <OptionalAuthGuard>
+            <Component {...pageProps} />
+          </OptionalAuthGuard>
+        </div>
+      </main>
       <Analytics />
     </SessionProvider>
   )
