1eef3a2167607f8939f2463aa53f69530294522b
Emilio Heinzmann <emiliosheinz@gmail.com>
2023-01-07 10:59:10 -0300

    feat: :sparkles: adds createNewDeck route

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,3 +1,4 @@
+import { decksRouter } from './routers/decks'
 import { createTRPCRouter } from './trpc'
 
 /**
@@ -5,6 +6,8 @@ import { createTRPCRouter } from './trpc'
  *
  * All routers added in /api/routers should be manually added here
  */
-export const appRouter = createTRPCRouter({})
+export const appRouter = createTRPCRouter({
+  decks: decksRouter,
+})
 
 export type AppRouter = typeof appRouter
