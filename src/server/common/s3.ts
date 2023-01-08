854e331d94df15a3de24115cc55178d2c5bd023a
Emilio Heinzmann <emiliosheinz@gmail.com>
2023-01-08 15:35:28 -0300

    feat: :sparkles: adds s3 related server common utils

---
+++ src/server/common/s3.ts
@@ -0,0 +1,29 @@
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { randomBytes } from 'node:crypto'
+
+import { env } from '~/env/server.mjs'
+
+function getAwsS3Client() {
+  return new S3Client({
+    region: env.AWS_S3_REGION,
+    credentials: {
+      accessKeyId: env.AWS_S3_ACCESS_KEY_ID,
+      secretAccessKey: env.AWS_S3_SECRET_ACCESS_KEY,
+    },
+  })
+}
+
+export async function getS3UploadURL(key: string) {
+  const command = new PutObjectCommand({
+    Key: key,
+    Bucket: env.AWS_S3_BUCKET,
+  })
+
+  return getSignedUrl(getAwsS3Client(), command, { expiresIn: 180 })
+}
+
+export function getRandomFileName() {
+  const rawBytes = randomBytes(16)
+  return rawBytes.toString('hex')
+}
