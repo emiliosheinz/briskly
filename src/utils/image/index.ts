01ae11e89847ae07ddc162bb993f684f27274b3f
Emilio Heinzmann <emiliosheinz@gmail.com>
2023-01-11 19:21:03 -0300

    feat: :sparkles: adds image compression

---
+++ src/utils/image/index.ts
@@ -0,0 +1,17 @@
+export async function compress(file?: File): Promise<File | undefined> {
+  if (!file) return
+
+  const { default: Compressor } = await import('compressorjs')
+
+  const compressorPromise = new Promise<File | Blob>((resolve, reject) => {
+    new Compressor(file, {
+      quality: 0.8,
+      maxHeight: 768,
+      maxWidth: 768,
+      success: resolve,
+      error: reject,
+    })
+  })
+
+  return await (compressorPromise as Promise<File>)
+}
