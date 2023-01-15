55b664b2e67f9b31b10057ed5ceaa2677a609e8b
Emilio Heinzmann <emiliosheinz@gmail.com>
2023-01-15 17:15:36 -0300

    feat: :sparkles: adds basic decks rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,53 @@
 import { type NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
+
+import { Image } from '~/components/image'
+import { api } from '~/utils/api'
 
 const Home: NextPage = () => {
+  const { isLoading, isError, data } = api.decks.getPublicDecks.useQuery({
+    page: 0,
+  })
+
+  const renderContent = () => {
+    if (isLoading) return 'Carregando...'
+    if (isError) return 'Deu erro!'
+
+    console.log(data)
+    return (
+      <div className='flex-w grid grid-cols-1 gap-5 sm:grid-cols-2'>
+        {data.map(deck => (
+          <Link
+            href='#'
+            key={deck.id}
+            className='flex flex-col overflow-hidden rounded-md border border-primary-900 bg-primary-50 shadow-md hover:bg-primary-100 lg:flex-row'
+          >
+            <div className='relative flex aspect-square w-full lg:w-2/5'>
+              <Image
+                fill
+                src={deck.image}
+                style={{ objectFit: 'cover' }}
+                alt={`${deck.title} image`}
+                sizes='(min-width: 1024px) 250px,
+                 (min-width: 640px) 50vw, 
+                 100vw'
+              />
+            </div>
+            <div className='flex flex-1 flex-col p-4'>
+              <h5 className='mb-2 text-2xl font-bold tracking-tight text-primary-900'>
+                {deck.title}
+              </h5>
+              <p className='mb-3 font-normal text-primary-900'>
+                {deck.description}
+              </p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    )
+  }
+
   return (
     <>
       <Head>
@@ -9,10 +55,7 @@ const Home: NextPage = () => {
         <meta name='description' content='The perfect Flashcards app' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div className='flex flex-col items-center'>
-        <h1>Home Page</h1>
-        <h2>Under Development!</h2>
-      </div>
+      <div className='flex flex-col items-center'>{renderContent()}</div>
     </>
   )
 }
