7540a85c0f9e38c575b0091d546dd880c7b1493b
Emilio Heinzmann <emiliosheinz@gmail.com>
2023-01-15 17:13:18 -0300

    refactor: :recycle: improves image compression and upload

--- src/server/common/s3.ts
+++ src/server/common/s3.ts
@@ -1,4 +1,8 @@
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { randomBytes } from 'node:crypto'
 
@@ -18,9 +22,20 @@ export async function getS3UploadURL(key: string) {
   const command = new PutObjectCommand({
     Key: key,
     Bucket: env.AWS_S3_BUCKET,
+    ContentType: 'image/jpg',
   })
 
-  return getSignedUrl(getAwsS3Client(), command, { expiresIn: 180 })
+  return getSignedUrl(getAwsS3Client(), command, { expiresIn: 30 })
+}
+
+export async function getS3ImageUrl(key: string) {
+  const command = new GetObjectCommand({
+    Key: key,
+    Bucket: env.AWS_S3_BUCKET,
+    ResponseContentType: 'image/jpg',
+  })
+
+  return getSignedUrl(getAwsS3Client(), command, { expiresIn: 60 * 30 })
 }
 
 export function getRandomFileName() {
