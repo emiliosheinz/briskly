3a84f15aa206a1b635560fffff5e2454e5af10b6
Emilio Heinzmann <emiliosheinz@gmail.com>
2023-01-15 17:10:44 -0300

    chore: :wrench: adds device sices to next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,22 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+!process.env.SKIP_ENV_VALIDATION && (await import('./src/env/server.mjs'))
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
   },
-};
-export default config;
+  images: {
+    // Follows TailwindCSS screens breakpoints
+    deviceSizes: [640, 768, 1024, 1280, 1536],
+    domains: [
+      `${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_S3_REGION}.amazonaws.com`,
+    ],
+  },
+}
+export default config
