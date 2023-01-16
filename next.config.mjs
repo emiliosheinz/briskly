a1d56e65fd7b2932ea4f1a075d1cb3323abdbf48
Emilio Heinzmann <emiliosheinz@gmail.com>
2023-01-16 19:42:07 -0300

    chore: :heavy_plus_sign: adds bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
+import bundleAnalyzer from '@next/bundle-analyzer'
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
 !process.env.SKIP_ENV_VALIDATION && (await import('./src/env/server.mjs'))
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -19,4 +25,4 @@ const config = {
     domains: [`${process.env.AWS_CLOUD_FRONT_URL?.replace('https://', '')}`],
   },
 }
-export default config
+export default withBundleAnalyzer(config)
